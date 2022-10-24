d6e9ac052da2ff0f69d53b0eee87b0e15d07ae1a
erinwood24 <ewood24@penguinhall.org>
2022-10-24 11:09:36 -0400

    FIRST CLASS

---
+++ refresher.js
@@ -0,0 +1,39 @@
+// //1
+// console.log('Hello world')
+
+// //2
+let message='How are you today?'
+// console.log(message)
+
+// //3
+let greeting='Hello '
+let name='Sue'
+// console.log(greeting + name + '!')
+
+// //4
+let age=13
+// console.log('You are '+ age)
+
+// //5
+// console.log('In ten years I will be '+ (age + 10))
+
+
+
+//Part 2
+
+//8-10
+if (name == 'Bob') {
+    console.log('I knew it was you, '+name+'!')
+}else{
+    console.log('Hey '+name+ ', you\'re not Bob!')
+}
+
+//11
+let iAmWearingABelt= true
+let iAmWearingGlasses= false
+
+if (iAmWearingABelt==true && iAmWearingGlasses==true){
+    console.log('probably not Mr. Gorton')
+}else(iAmWearingABelt==true && iAmWearingGlasses==false){
+    console.log('Probably Mr. Gorton')
+}
