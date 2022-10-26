c4ce4925cd007586ca5c3016ca7733414ac33fad
erinwood24 <ewood24@penguinhall.org>
2022-10-26 09:53:57 -0400

    refresher.js classwork

--- refresher.js
+++ refresher.js
@@ -2,16 +2,16 @@
 // console.log('Hello world')
 
 // //2
-let message='How are you today?'
+let message = 'How are you today?'
 // console.log(message)
 
 // //3
-let greeting='Hello '
-let name='Sue'
+let greeting = 'Hello '
+let name = 'Sue'
 // console.log(greeting + name + '!')
 
-// //4
-let age=13
+//4
+// let age = 13
 // console.log('You are '+ age)
 
 // //5
@@ -21,7 +21,7 @@ let age=13
 
 //Part 2
 
-//8-10
+// 8-10
 // if (name == 'Bob') {
 //     console.log('I knew it was you, '+name+'!')
 // }else{
@@ -29,16 +29,16 @@ let age=13
 // }
 
 // //11
-// let iAmWearingABelt= false
-// let iAmWearingGlasses= false
-// if(iAmWearingABelt==true && iAmWearingGlasses==false){
-//     console.log('Probably Mr. Gorton')
-// }
+let iAmWearingABelt = false
+let iAmWearingGlasses = false
+if (iAmWearingABelt == true && iAmWearingGlasses == false) {
+  console.log('Probably Mr. Gorton')
+}
 
 
-// if (iAmWearingABelt==true && iAmWearingGlasses==true){
-//     console.log('probably not Mr. Gorton')
-// }
+if (iAmWearingABelt == true && iAmWearingGlasses == true) {
+  console.log('probably not Mr. Gorton')
+}
 
 // if (iAmWearingABelt==false){
 //     console.log('Probably just some dude')
@@ -46,10 +46,38 @@ let age=13
 
 //Part 3
 
-for (let i = 0; i < 6; i++) {
-    console.log('comp sci is awesome')
-  }
+// for (let i = 0; i < 6; i++) {
+//   console.log('comp sci is awesome')
+// }
 
-  for (let i = 0; i < 11; i++) {
-    console.log(i)
-  }
+// for (let i = 0; i < 11; i++) {
+//   console.log(i)
+// }
+
+// //16
+// tellMeAge(14)
+
+// function tellMeAge (age){
+// console.log('You are '+age+' years old.')
+// }
+
+//18
+// makePizza('pepperoni', 'small')
+// makePizza('cheese', 'large')
+
+// function makePizza(type, size) {
+//   console.log('One ' + size + ' ' + type + ' pizza coming up!')
+//   console.log('That will be $' + pricePizza(size) + '!')
+// }
+
+// function pricePizza(size) {
+//   let answer
+//   if (size == 'small') {
+//     answer=13
+//   }else{
+//     answer=17
+//   }
+//   return answer
+// }
+
+
