3d3e50abbe917e421566bc263d044b78f07f9504
erinwood24 <ewood24@penguinhall.org>
2022-11-06 00:30:12 -0400

    Bobtalk homework

--- Bop-Talk/bopTalk.js
+++ Bop-Talk/bopTalk.js
@@ -1,14 +1,17 @@
 let myString = 'This is my bobtalk game. I hope you enjoy!'
 let consonant =['p','b','c','d','f','g','h','j','k','l','m','n','q','r','s','t','v','w','x','y','z']
 
-
+//write origalal string
 myString = myString.toLowerCase()
 console.log(myString)
 
-
+//translat to bobTalk
 for(let i = 0; i < consonant.length; i++){
-    myString = myString.replaceAll( consonant[i], ' ' + consonant[i] + 'op ')
+    myString = myString.replaceAll( consonant[i], consonant[i] + 'op ')
 }
+console.log(myString)
 
 
+//code cracker 
+myString = myString.replaceAll( 'op ', '')
 console.log(myString)
